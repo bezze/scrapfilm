a91901df49c8b1d638b323cb35c0743e2ce3a019
Bezze <franbcki@gmail.com>
2017-02-03 18:47:11 -0300

    Fixing diff_phase

--- gather_kura.py
+++ gather_kura.py
@@ -24,24 +24,30 @@ path = '../'
 
 import numpy as np
 import init_var as iv
-from calc_phase import phase
+from calc_phase import phase, diff_phase
 hfchain = int(iv.chains*.5)
 for tb in [ 0, hfchain ]: # 0 -> bottom, 60 -> top
     for row in range(3):
         fi_matrix, kura = phase(mode, 0+tb, hfchain+tb, row)
+        dfi_matrix = diff_phase( fi_matrix )
         filename_k = path+'kura.row_'+str(int(row+3*tb/hfchain))+'_'+mode+'.npy'
         filename_fi = path+'fi.row_'+str(int(row+3*tb/hfchain))+'_'+mode+'.npy'
+        filename_dfi = path+'dfi.row_'+str(int(row+3*tb/hfchain))+'_'+mode+'.npy'
         try:
             old_data_k = np.load(filename_k)
             old_data_fi = np.load(filename_fi)
+            old_data_dfi = np.load(filename_dfi)
             joined_k = np.hstack( (old_data_k, kura) )
             joined_fi = np.hstack( (old_data_fi, fi_matrix) )
+            joined_dfi = np.hstack( (old_data_dfi, dfi_matrix) )
             np.save(filename_k, joined_k)
             np.save(filename_fi, joined_fi)
+            np.save(filename_dfi, joined_dfi)
             print('Data appended to '+filename_k+' and '+filename_fi)
         except FileNotFoundError:
             print('File not found')
             np.save(filename_k, kura)
             np.save(filename_fi, fi_matrix)
+            np.save(filename_dfi, dfi_matrix)
             print('Starting new file at '+ filename_k+' and '+filename_fi)
 
--- stat_kura.py
+++ stat_kura.py
@@ -14,44 +14,53 @@ if not os.path.isdir('./movie'):
 for i in range(6): # 0 -> bottom, 60 -> top
     filename_k = path+'kura.row_'+str(i)+'_'+mode+'.npy'
     filename_fi = path+'fi.row_'+str(i)+'_'+mode+'.npy'
+    filename_dfi = path+'dfi.row_'+str(i)+'_'+mode+'.npy'
     if i==0:
         dummy_k = np.load(filename_k) #all_kura[i,:]
         dummy_fi = np.load(filename_fi) #all_kura[i,:]
+        dummy_dfi = np.load(filename_dfi) #all_kura[i,:]
         all_kura = np.empty([6,dummy_k.size], dtype=complex)
         matsize = [x for x in dummy_fi.shape]; fisize=[6]
         fisize.extend(matsize)
         all_fi = np.empty(fisize)
+        all_dfi = np.empty(fisize)
         #print(all_kura.shape)
         #print(dummy[0:10])
 
     all_kura[i,:] = np.load(filename_k) 
     all_fi[i,:,:] = np.load(filename_fi) 
+    all_dfi[i,:,:] = np.load(filename_dfi) 
 
 order = np.absolute(all_kura)
 psi = np.angle(all_kura)
 
-print(all_fi.shape)
-
 import matplotlib as mpl
-mpl.use('Agg')
+#mpl.use('Agg')
 from matplotlib import pyplot as plt
-fase=0
-for time in range(all_fi.shape[2]):#range(1):# 
-    fig, ax = plt.subplots(2,3,subplot_kw=dict(projection='polar'))
-    for row in range(3):
-        for chain in range(all_fi.shape[1]):
-            #ax.plot( np.cos(s), np.sin(s),'b')
-            for ud in range(2):
-                place=row+3*ud
-                ax[ud,row].set_aspect('equal')
-                ax[ud,row].plot(all_fi[place,chain,time]+fase,1,
-                         marker='^', color='r', markersize=10 )
+for chain in range(all_dfi.shape[1]):
+    plt.plot(all_dfi[0,chain,:],color=)
+plt.show()
 
-    fig.suptitle('frame '+str(time), fontsize=14, fontweight='bold',
-                 color='red')
-    fig.tight_layout()
-    fig.savefig('movie/frame_'+str(time)+'.png',format='png')
-    plt.close(fig)
+#import matplotlib as mpl
+#mpl.use('Agg')
+#from matplotlib import pyplot as plt
+#fase=0
+#for time in range(all_fi.shape[2]):#range(1):# 
+#    fig, ax = plt.subplots(2,3,subplot_kw=dict(projection='polar'))
+#    for row in range(3):
+#        for chain in range(all_fi.shape[1]):
+#            #ax.plot( np.cos(s), np.sin(s),'b')
+#            for ud in range(2):
+#                place=row+3*ud
+#                ax[ud,row].set_aspect('equal')
+#                ax[ud,row].plot(all_fi[place,chain,time]+fase,1,
+#                         marker='^', color='r', markersize=10 )
+#
+#    fig.suptitle('frame '+str(time), fontsize=14, fontweight='bold',
+#                 color='red')
+#    fig.tight_layout()
+#    fig.savefig('movie/frame_'+str(time)+'.png',format='png')
+#    plt.close(fig)
 
 #for row in range(order.shape[0]):
 #    plt.plot(order[row,:], label='row '+str(row))
