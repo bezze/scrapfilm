514c7f35c277833df0dfaa0b0e77aca54b3d8732
Francisco Bezzecchi <franbcki@gmail.com>
2017-03-16 15:44:52 -0300

    Calculando histograma de x y v por separadoç

--- calc_hist.py
+++ calc_hist.py
@@ -13,6 +13,20 @@ all_data = np.load(path)
 x_data = all_data[0,:]
 v_data = all_data[1,:]
 
+def ed2cen(ed):
+    cen = .5*(ed[1:]+ed[:-1])
+    return cen
+
+Hx, xed = np.histogram(x_data, bins=nbin)
+Hv, ved = np.histogram(v_data, bins=nbin)
+
+fig, ax = plt.subplots(1,2)
+fig.suptitle(name+'x_and_v.bins_'+str(nbin))
+ax[0].plot(ed2cen(xed),Hx, '-xr', label="x");ax[0].legend()
+ax[1].plot(ed2cen(ved),Hv, '-sk', label="v");ax[1].legend()
+plt.savefig(name+'x_and_v.bins_'+str(nbin)+'.png', format='png')
+plt.clf()
+
 H, xedges, yedges = np.histogram2d(x_data, v_data, bins=nbin)
 X, Y = np.meshgrid(xedges, yedges)
 plt.pcolormesh(X, Y, H)
