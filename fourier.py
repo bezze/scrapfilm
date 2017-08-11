2ee558f85c36c3b8fbaf0e9a8c98b52f383c28d0
Bezze <franbcki@gmail.com>
2017-08-10 21:48:19 -0300

    fourier ahora funciona, toma fort.60 y devuelve rfft's de cada coord. Chequear sampling_rate (d)

--- fourier.py
+++ fourier.py
@@ -35,29 +35,24 @@ pad=np.zeros((pot-size,datos_ar.shape[1]-1))
 energias_pad=np.vstack((datos_ar[:,1:],pad)) #datos2=np.append(np.asarray(datos),pad)
 t_fft=t.clock()
 
-freq = np.fft.rfftfreq(energias_pad.shape[0],d=1) #+str(freq[i])+' '
+freq = np.fft.rfftfreq(energias_pad.shape[0],d=10) #+str(freq[i])+' '
+
+with open("transformadas.dat", "w") as g:
+    for i in range(len(freq)):
+        g.write( str(freq[i]) +'\n')
+
 for col in range(energias_pad.shape[1]):
     fft = np.fft.rfft(energias_pad[:,col], pot)
 
-    lista = [ x for x in range(10)]
-    
-    if not os.path.isfile("transformadas.dat"):
-        with open("transformadas.dat", "w+") as w:
-            for number in fft:
-                w.write(str(number)+'\n')
-    else:
-        with fileinput.FileInput("transformadas.dat",inplace=1) as f:
-            for line,elemento in zip(f,fft):
-                line = line.strip('\n') +' '+ str(elemento)
-                print(line)
+    with fileinput.FileInput("transformadas.dat",inplace=10) as f:
+        for line,elemento in zip(f,fft):
+            line = line.strip('\n') +' '+ str(np.real(elemento))+' '+str(np.imag(elemento))+' '+ str(np.absolute(elemento))
+            print(line)
 
 #print(t.clock()-t_fft)
 
 #-------------------------
 
-with open("transformadas.dat", "w") as g:
-    for i in range(len(freq)):
-        g.write(freq[i], fft[i,:])
 
 #for element in fft[20:]:
 #    print(element**2)
