ef71bca13c3f1dcf05bbf62703e56a7036f5b662
Bezze <franbcki@gmail.com>
2017-10-05 22:30:03 -0300

    arreglado stack_r_all

--- stack_r_all.py
+++ stack_r_all.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys , os
+import numpy as np
 
 try:
     mode = sys.argv[1]
@@ -17,14 +18,13 @@ except IndexError:
 
     raise SystemExit
 
-import init_var as iv
-import numpy as np
 
 path = '../'
-N = 400 # Runs number
+N = 40 # Runs number
 
 for i in range(1,N+1): # 1_runs, 2_runs, ..., N_runs
     os.chdir(str(i)+'_run')
+    import init_var as iv
 
     if mode == "cm":
         """ Centre of Mass  """
@@ -40,13 +40,13 @@ for i in range(1,N+1): # 1_runs, 2_runs, ..., N_runs
         dummy_x = iv.r_cent[:,:,9,0]
         dummy_vx = iv.vcm[:,:,0]
     
-    new_data = np.vstack(( dummy_x, dummy_vx ))
+    new_data = np.stack(( dummy_x, dummy_vx ), axis=2)
     
     filename = path+'stack_all_'+mode+'.npy'
     
     try:
         old_data = np.load(filename)
-        joined_data = np.hstack( (old_data, new_data) )
+        joined_data = np.concatenate( (old_data, new_data), axis=0 )
         np.save(filename, joined_data)
         print('Data appended to '+ filename)
     except FileNotFoundError:
